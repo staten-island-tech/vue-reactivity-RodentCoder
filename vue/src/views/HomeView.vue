6fc8aab12bed018841afa7d4b8b9204bc1748711
RodentCoder <ichen080405@gmail.com>
2023-03-10 13:14:34 -0500

    More titual work

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,5 +1,33 @@
-<script setup></script>
-
 <template>
-  <h1>Heello</h1>
+  <div class="Home">
+    <h1 v-if="alive">{{ students }} is a person</h1>
+    <h1 v-else="alive">{{ students }} is not a person</h1>
+    <h2>things to steal from him</h2>
+    <ui>
+      <li v-for="parts in vulablebodyparts" :key="parts">{{ parts }}</li>
+    </ui>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      students: "Daniel",
+      vulablebodyparts: ["brain", "liver", "lungs", "kidney"],
+      alive: true,
+    };
+  },
+  methods: {
+    alive: function () {
+      if (this.alive === true) {
+        this.alive = false;
+      } else {
+        this.alive = true;
+      }
+    },
+  },
+};
+</script>
